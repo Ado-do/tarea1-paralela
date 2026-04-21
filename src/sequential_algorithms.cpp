8e8ca165c4f8c9926c00b0260c63caa39bd2118f
adodo <alonbustos2021@udec.cl>
2026-04-20 23:00:20 -0400

    add sequential classic multiply

--- src/sequential_algorithms.cpp
+++ src/sequential_algorithms.cpp
@@ -1,7 +1,19 @@
 #include "sequential_algorithms.hpp"
 
 void sequential_classic_multiply(const Matrix& A, const Matrix &B, Matrix &C) {
-    // implementar
+    size_t n = A.size();
+
+    // iterar filas de A
+    for (size_t i = 0; i < n; i++) {
+        // iterar columnas de B
+        for (size_t j = 0; j < n; j++) {
+            double sum = 0.0;
+            for (size_t k = 0; k < n; k++) {
+                sum += A(i, k) * B(k, j);
+            }
+            C(i, j) = sum;
+        }
+    }
 }
 
 void sequential_cachefriendly_multiply(const Matrix& A, const Matrix &B, Matrix &C, size_t b) {
--- src/sequential_main.cpp
+++ src/sequential_main.cpp
@@ -1,8 +1,39 @@
+#include <iomanip>
 #include <iostream>
 
 #include "sequential_algorithms.hpp"
 
+using namespace std;
+
+void printMatrix(const string& name, Matrix& M, size_t n) {
+    cout << "* " << name << ":\n";
+    for (size_t i = 0; i < n; i++) {
+        cout << "[ ";
+        for (size_t j = 0; j < n; j++) {
+            cout << setw(3) << M(i, j) << " ";
+        }
+        cout << " ]\n";
+    }
+    cout << endl;
+}
+
 int main() {
     std::cout << "Hello World!\n";
+
+    size_t n = 2;
+    Matrix A(n), B(n), C(n);
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n; j++) {
+            A(i, j) = B(i, j) = (i * n) + j + 1;
+        }
+    }
+
+    printMatrix("A", A, n);
+    printMatrix("B", B, n);
+
+    sequential_classic_multiply(A, B, C);
+
+    printMatrix("C", C, n);
+
     return 0;
 }
